0e5e88c5b686863502fda97d6d5b3195ab1421bf
Jakub Ostrowicki <kuba.ostrowicki@interia.pl>
2014-06-08 17:41:56 +0200

    artists and playcount lists

--- app/artists.py
+++ app/artists.py
@@ -3,7 +3,8 @@ import json
 from django.http import HttpResponse
 import re
 #from urllib2 import urlopen
-
+# leepic - artysci
+# doooitz - playcount
 class ArtistParser:
 
 	API_KEY = '93903103571bcd8ca15d664cbe16d6c9'
@@ -13,13 +14,21 @@ class ArtistParser:
 		parameters = {'method' : 'user.gettopartists', 'user': name, 'api_key': ArtistParser.API_KEY, 'format' : 'json'}
 		result = requests.get(ArtistParser.API_URL,params=parameters)
 		resultdict = json.loads(result.content.decode("utf-8"))
-		response = json.loads(result.content.decode("utf-8"))
-		regx=re.compile('<name>(.*)</name>')
-		names=regx.findall(str(response))
+		regx = re.compile('\'name\': \'(\w*)\',')
+		leepic = regx.findall(str(resultdict))
+		regx2 = re.compile('\'playcount\': \'(\d*)\',')
+		doooitz = regx2.findall(str(resultdict))
+		#response = json.loads(result.content.decode("utf-8"))
+		#regx=re.compile('<name>(.*)</name>')
+		#regx=re.compile('rumcajsss')
+		#names=regx.findall(str(response))
 
 
-		#resultdict = json.loads(result.content.decode("utf-8"))
-		#regx=re.compile('<name>(.*)</name>') 
-		return resultdict.items()
+#resultdict = json.loads(result.content.decode("utf-8"))
+#regx=re.compile('<name>(.*)</name>')
+		lol = leepic[0] + doooitz[0]
+		#return doooitz
+		return lol
+		#return resultdict.items()
 		#return HttpResponse(resultdict)
-		#.decode("utf-8")
+#.decode("utf-8")
--- app/views.py
+++ app/views.py
@@ -10,33 +10,33 @@ class IndexView(View):
         name1=request.GET.get('name1','rumcajsss')
         #name2=request.GET.get('name2','rumcajsss')
         template=Template("""
-    		<html>
-    			<head>
-    				<title>Comparision of top artists of two given users.</title>
-    			</head>
-    			<body>
-    				<form>{% csrf_token %}
-    				User 1: <input type="text" name="name1"><br>
-    				<input type="submit" value="Submit">
-    				</form>
-                    
+<html>
+<head>
+<title>Comparision of top artists of two given users.</title>
+</head>
+<body>
+<form>{% csrf_token %}
+User 1: <input type="text" name="name1"><br>
+<input type="submit" value="Submit">
+</form>
 
-                    wtf
-    			</body>
-    		</html>
-		""")
+wtf
+</body>
+</html>
+""")
 
-        user1=ArtistParser.download('rumcajsss')
+        #user1=ArtistParser.download('rumcajsss')
         #user2=ArtistParser()
         #ArtistParser.download()
-        context1=Context(ArtistParser.download('rumcajsss'))
+        #context1=Context(ArtistParser.download('rumcajsss'))
         #print (ArtistParser.download())
         #print (context1)
         #return HttpResponse(template.render(context1))
         #return HttpResponse(name1)
         #print (ArtistParser.download('rumcajsss'))
         zmienna = ArtistParser.download('rumcajsss')
-        context = Context(ArtistParser.download(name1))
+        zmienna2 = ArtistParser.download('msitake')
+        #context = Context(ArtistParser.download(name1))
         #return HttpResponse(template.render(context))
         
-        return HttpResponse(zmienna)
+        return HttpResponse(zmienna2)
